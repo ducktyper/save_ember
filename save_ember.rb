e99ecfbd852460f4ec35ebf07603116c2a8d87d5
Ducksan Cho <ducktyper@gmail.com>
2017-03-27 23:40:04 +1300

    Fix nested match leads to incorrect $1 also refactoring

--- save_ember.rb
+++ save_ember.rb
@@ -4,6 +4,7 @@ include  Common
 
 ROOT = "/Users/ducksan/projects/hermes/app/assets/javascripts/"
 DEST = "/Users/ducksan/projects/hermes-js/app/"
+APP  = "Hermes"
 # DEST = "/Users/ducksan/projects/save_ember/"
 
 def create_paths
@@ -13,29 +14,13 @@ def create_paths
   end
 end
 
-def extended_model js, basename
-  class_name = class_name_from basename
-
-  if js.sub!(/Hermes\.#{class_name} = Hermes\.(\w+)\.extend\(/, 'export default \1.extend(')
-    js.prepend "import #{$1} from './#{file_name_from $1}';\n"
-  end
-  js
-end
-
-def export_default js
-  js.sub!(/\s*Hermes\.\w+ = DS.Model.extend/, "export default DS.Model.extend")
-  js
-end
-
-def require_model js
-  js.prepend "import DS from 'ember-data';\n"
-end
-
 def extract_serializer js, basename
-  if js.sub!(/\s*(Hermes\.\w+ = DS.ActiveModelSerializer.extend#{BRACKET};)\s*/, '')
-    serializer = $1
-    serializer.sub!(/\s*Hermes\.\w+ = DS/, 'export default DS')
-    serializer = require_model serializer
+  pattern = /\s*(?<serializer>Hermes\.\w+ = DS.ActiveModelSerializer.extend#{BRACKET};)\s*/
+  if (match = js.match pattern)
+    js.sub! pattern, ''
+
+    serializer = match[:serializer]
+    serializer = export_class APP, serializer
 
     save serializer, "#{DEST}serializers/#{basename}.js"
   end
@@ -43,15 +28,12 @@ def extract_serializer js, basename
 end
 
 def extract_adapter js, basename
-  if js.sub!(/\s*Hermes\.\w+ = Hermes\.InternalAdapter.extend\(\)\s*/, '')
-    adapter = "export default DS.RESTAdapter.extend({\n  namespace: 'internal/v1'\n});"
-    adapter = require_model adapter
+  pattern = /\s*(?<adapter>#{APP}\.\w+ = (?<name>[\w\.]+)Adapter.extend#{BRACKET};)\s*/
+  if (match = js.match pattern)
+    js.sub! pattern, ''
 
-    save adapter, "#{DEST}adapters/#{basename}.js"
-  elsif js.sub!(/\s*(Hermes\.\w+ = Hermes\.RESTAdapter.extend#{BRACKET};)\s*/, '')
-    adapter = $1
-    adapter.sub!(/\s*Hermes\.\w+ = Hermes/, 'export default DS')
-    adapter = require_model adapter
+    adapter = match[:adapter]
+    adapter = export_class APP, adapter
 
     save adapter, "#{DEST}adapters/#{basename}.js"
   end
@@ -70,9 +52,7 @@ create_paths
 
     js = CoffeeScript.compile File.read(file)
     js = remove_outer_closer js
-    js = extended_model js, basename
-    js = export_default js
-    js = require_model js
+    js = export_class APP, js
     js = extract_serializer js, basename
     js = extract_adapter js, basename
 
