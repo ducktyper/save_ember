1da3859622d9ce708df9b861cb88c7e3e9faa95d
Ducksan Cho <ducktyper@gmail.com>
2017-03-27 22:52:26 +1300

    Add require_class method

--- common.rb
+++ common.rb
@@ -3,7 +3,7 @@ module Common
 
   def file_name_from class_name
     class_name.sub(/^[A-Z]/, &:downcase).
-      gsub(/[A-Z]/) {|first| '-' + first.downcase}
+      gsub(/[A-Z]/) {|first| '-' + first.downcase}.gsub(/-(\w)(?=-)/, '\1')
   end
 
   def class_name_from file_name
@@ -21,4 +21,13 @@ module Common
     js
   end
 
+  def require_class app_name, class_name
+    if class_name =~ /^DS\./
+      "import DS from 'ember-data';\n"
+    elsif class_name =~ /^#{app_name}\.(\w+)$/
+      "import #{$1} from './#{file_name_from $1}';\n"
+    else
+      puts "Don't know how to import #{class_name}"
+    end
+  end
 end
--- common_test.rb
+++ common_test.rb
@@ -4,6 +4,8 @@ require './common'
 include Common
 
 describe "common" do
+  let(:app) {'Hermes'}
+
   it "bracket" do
     [
       ["func1({abcde;});", "({abcde;})"],
@@ -17,10 +19,11 @@ describe "common" do
 
   it "file_name_from & class_name_from" do
     [
-      ["AaBb", "aa-bb"],
-    ].each do |class_name, file_name|
+      ["AaBb", "aa-bb", "AaBb"],
+      ["AABb", "aa-bb", "AaBb"],
+    ].each do |class_name, file_name, back_to_class_name|
       assert_equal(file_name, file_name_from(class_name))
-      assert_equal(class_name, class_name_from(file_name))
+      assert_equal(back_to_class_name, class_name_from(file_name))
     end
   end
 
@@ -30,5 +33,10 @@ describe "common" do
   code
 }).call(this);"
     assert_equal(remove_outer_closer(from), "code")
+  end
+
+  it "require_class" do
+    assert_equal("import DS from 'ember-data';\n", require_class('Hermes', 'DS.Model'))
+    assert_equal("import RESTAdapter from './rest-adapter';\n", require_class('Hermes', 'Hermes.RESTAdapter'))
   end
 end
